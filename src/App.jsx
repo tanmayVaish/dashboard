21e2483fcc3fa43cba12e20dc16156dead5aa1ed
tanmayVaish <tanmay777leon@gmail.com>
2022-02-04 00:00:09 +0530

    image and video added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import './App.css'
 import axios, {Axios} from "axios";
 
@@ -9,7 +9,9 @@ function App() {
   const [subtype, setSubtype] = useState(null);
   const [from , setFrom] = useState('2010-08-01T00:00:00');
   const [to , setTo] = useState('2021-09-01T00:00:00');
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
+  const [images, setImages] = useState([]);
+  const [video, setVideo] = useState([]);
 
   const fetchData = async (e) => {
     e.preventDefault();
@@ -19,12 +21,60 @@ function App() {
       vehicle_subtype: subtype,
       datefrom: from,
       dateto: to,
-    }).then(function (response) {
-      console.log(response.data.data);
+    }).then(async function (response) {
       setData(response.data.data);
+      await fetchImage(response);
+      await fetchVideo(response);
     }).catch(function (error) {
       console.log(error);
     });
+  }
+
+
+
+  const fetchVideo = async (d) => {
+    const url = []
+    for(let i=0;i<d.data.data.length;i++) {
+      await fetch('https://tolltax.xyz/demoapi/get_videoclip', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+          filepath: d.data.data[i].videoclip,
+        })
+      }).then((response) => {
+        return response.blob();
+      }).then((res) => {
+        url.push(URL.createObjectURL(res));
+        // setVideo([...video, URL.createObjectURL(res)]);
+      });
+    }
+    setVideo(url);
+  }
+
+
+
+  const fetchImage = async (d) => {
+    const url = []
+    for(let i=0;i<d.data.data.length;i++) {
+      await fetch('https://tolltax.xyz/demoapi/get_file', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+          filepath: d.data.data[i].image,
+        })
+      }).then((response) => {
+        return response.blob();
+      }).then((res) => {
+        url.push(URL.createObjectURL(res));
+      });
+    }
+    setImages(url);
   }
 
   return (
@@ -47,27 +97,41 @@ function App() {
           {data && data.map((item, index) => {
             return (
                 <div className={'dataItem'} key={index}>
-                  <div className={'dataItemImage'}>
-                    <img src={`https://tolltax.xyz${item.image}`} alt={item.image}/>
-                  </div>
                   <div className={'dataItemTitle'}>
-                    <span>{item.licence_plate}</span>
+                    Vehicle Number: {index+1}
+                  </div>
+                  <div className={'dataItemImage'}>
+                    Image
+                    <img src={images[index]} alt={'truck'}/>
                   </div>
                   <div className={'dataItemContent'}>
                     <div className={'dataItemContentItem'}>
+                      License Plate No.:
+                      <span>{item.licence_plate}</span>
+                    </div>
+                    <div className={'dataItemContentItem'}>
+                      Vehicle Type:
                       <span>{item.vehicle_type}</span>
                     </div>
                     <div className={'dataItemContentItem'}>
-                      <span>{item.vehicle_subtype}</span>
+                      Vehicle Subtype:
+                      <span>{(item.vehicle_subtype)?item.vehicle_subtype:"Not Available"}</span>
                     </div>
                     <div className={'dataItemContentItem'}>
-                      <span>{item.datefrom}</span>
+                      Lane No.:
+                      <span>{(item.lane_no)?item.lane_no:"Not Available"}</span>
                     </div>
                     <div className={'dataItemContentItem'}>
-                      <span>{item.dateto}</span>
+                      Job ID:
+                      <span>{(item.jobid)?item.jobid:"Not Available"}</span>
                     </div>
                     <div className={'dataItemContentItem'}>
-                      <span>{item.total_amount}</span>
+                      Time:
+                      <span>{item.time}</span>
+                    </div>
+                    <div className={'dataItemContentItem'}>
+                      Video
+                      <video src={video[index]} controls/>
                     </div>
                   </div>
                 </div>
